69df7a0151f291af8c7ca05d57660952a81faad0
8358rahul <rahulshanisare@gmail.com>
2025-07-25 13:41:32 +0530

    final issue resolve

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function cnForSlider(...classes: (string | boolean | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
-}
+ 
