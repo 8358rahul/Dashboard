39573a0021e95a1af573c69bf4c409adfb4db56d
8358rahul <rahulshanisare@gmail.com>
2025-07-25 18:10:52 +0530

    logo fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,7 +13,7 @@ import {
   ChevronsUpDown,
   UserIcon,
   SettingsIcon,
-  BookOpenCheck, 
+  BookOpenCheck,
   Circle,
   LogOutIcon,
 } from "lucide-react";
@@ -39,18 +39,27 @@ export default function Sidebar() {
       {/* top view */}
       <div>
         <div className="text-xl font-bold mb-6 flex items-center justify-between">
-          <img src="/logo.svg" alt="Logo" className="h-8 w-auto" loading="lazy"/>
-                  <ThemeToggle />
-          
+          <div className="flex items-center">
+          <img
+            src="/fav.svg"
+            alt="Logo"
+            className="h-7 w-auto"
+            loading="lazy"
+          />
+           <span className="font-bold tracking-widest uppercase text-lg text-gray-900 dark:text-white mb-0.5 ml-0.5"
+>
+            PRISM
+          </span> 
+          </div>
+          <ThemeToggle />
         </div>
         <div className="relative mb-6">
           <Search className="absolute left-3 top-2.5 h-4 w-4 text-muted-foreground" />
           <input
             type="text"
             placeholder="Search"
-              className="pl-8 pr-20 py-2 w-full rounded-md border border-border text-sm text-foreground bg-background cursor-pointer"
-
-             disabled={true}
+            className="pl-8 pr-20 py-2 w-full rounded-md border border-border text-sm text-foreground bg-background cursor-pointer"
+            disabled={true}
           />
           <kbd className="absolute right-3 top-2.5 pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 py-0.5 text-xs font-mono text-muted-foreground opacity-100">
             <span className="text-xs">⌘</span>K
@@ -140,7 +149,6 @@ export default function Sidebar() {
             side="right"
             align="start"
             sideOffset={4}
-           
             className="w-72 gap-1"
           >
             <DropdownMenuLabel className="flex items-center">
@@ -152,82 +160,82 @@ export default function Sidebar() {
             </DropdownMenuLabel>
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                  <SettingsIcon className="mr-1 h-4 w-4" />
+                <SettingsIcon className="mr-1 h-4 w-4" />
                 Account settings
-                 <kbd className="absolute right-3 top-2.5 pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 py-0.5 text-xs font-mono text-muted-foreground opacity-100">
-                <span className="text-xs">⌘S</span>
-              </kbd>
-                </DropdownMenuItem>
+                <kbd className="absolute right-3 top-2.5 pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 py-0.5 text-xs font-mono text-muted-foreground opacity-100">
+                  <span className="text-xs">⌘S</span>
+                </kbd>
+              </DropdownMenuItem>
               <DropdownMenuItem>
-                  <BookOpenCheck className="mr-1 h-4 w-4" />
-                Documentation</DropdownMenuItem>
+                <BookOpenCheck className="mr-1 h-4 w-4" />
+                Documentation
+              </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuLabel>Switch account</DropdownMenuLabel>
-            <DropdownMenuItem> 
-               <Button
-              variant="ghost"
-              className="w-full justify-start py-6 px-2"
-            >
-              <Avatar className="w-8 h-8">
-                <AvatarImage src={"/userLogo.png"} />
-                <AvatarFallback>OR</AvatarFallback>
-              </Avatar>
-              <div className="flex-1 text-left">
-                <div className="text-sm mb-1 font-medium leading-none text-foreground dark:text-foreground">
-                  Olivia Rhye
+            <DropdownMenuItem>
+              <Button
+                variant="ghost"
+                className="w-full justify-start py-6 px-2"
+              >
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src={"/userLogo.png"} />
+                  <AvatarFallback>OR</AvatarFallback>
+                </Avatar>
+                <div className="flex-1 text-left">
+                  <div className="text-sm mb-1 font-medium leading-none text-foreground dark:text-foreground">
+                    Olivia Rhye
+                  </div>
+                  <div className="text-xs leading-none text-muted-foreground">
+                    olivia@untitledui.com
+                  </div>
                 </div>
-                <div className="text-xs leading-none text-muted-foreground">
-                  olivia@untitledui.com
-                </div>
-              </div> 
                 <Avatar className="w-4 h-4">
-                 <AvatarImage src={"/Checkbox.png"} />
-                 </Avatar>
-            </Button>
+                  <AvatarImage src={"/Checkbox.png"} />
+                </Avatar>
+              </Button>
             </DropdownMenuItem>
             <DropdownMenuItem>
-             <Button
-              variant="ghost"
-              className="w-full justify-start py-6 px-2"
-            >
-              <Avatar className="w-8 h-8">
-                <AvatarImage src={"/secondUser.png"} />
-                <AvatarFallback>OR</AvatarFallback>
-              </Avatar>
-              <div className="flex-1 text-left">
-                <div className="text-sm mb-1 font-medium leading-none text-foreground dark:text-foreground">
-               Sienna Hewitt
+              <Button
+                variant="ghost"
+                className="w-full justify-start py-6 px-2"
+              >
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src={"/secondUser.png"} />
+                  <AvatarFallback>OR</AvatarFallback>
+                </Avatar>
+                <div className="flex-1 text-left">
+                  <div className="text-sm mb-1 font-medium leading-none text-foreground dark:text-foreground">
+                    Sienna Hewitt
+                  </div>
+                  <div className="text-xs leading-none text-muted-foreground">
+                    sienna@untitledui.com
+                  </div>
                 </div>
-                <div className="text-xs leading-none text-muted-foreground">
-                  sienna@untitledui.com
-                </div>
-              </div>
-              <Circle className="ml-auto h-4 w-4" />
-            </Button>
+                <Circle className="ml-auto h-4 w-4" />
+              </Button>
             </DropdownMenuItem>
             <DropdownMenuItem>
-                <Button
-              variant="outline"
-              className="w-full justify-center py-4 px-2"
-            >
-              + Add account
-            </Button>
+              <Button
+                variant="outline"
+                className="w-full justify-center py-4 px-2"
+              >
+                + Add account
+              </Button>
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem className="cursor-pointer">
-                <LogOutIcon className="h-4 w-4" />
+              <LogOutIcon className="h-4 w-4" />
               Sign out
-                <kbd className="absolute right-3 top-2.5 pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 py-0.5 text-xs font-mono text-muted-foreground opacity-100">
+              <kbd className="absolute right-3 top-2.5 pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 py-0.5 text-xs font-mono text-muted-foreground opacity-100">
                 <span className="text-xs">
                   <span className="text-xs">⌘⬆Q</span>
                 </span>
               </kbd>
-              </DropdownMenuItem>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-      
     </aside>
   );
 }
